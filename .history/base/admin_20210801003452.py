36ffbe2c4ff3948cb17d39e239c435028ca575f7
hadang <hadh3101199@gmail.com>
2021-08-01 13:39:37 +0700

    changed sth

---
+++ .history/base/admin_20210801003452.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Project, Skill, Tag, Message, Endorsement
+
+# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Tag)
+admin.site.register(Message)
+admin.site.register(E)
